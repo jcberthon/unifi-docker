a368e8928e2c8db80865408329c4c9bf27162a6f
Jean-Christophe Berthon <huygens@berthon.eu>
2017-09-09 10:43:53 +0200

    Add support for future multi-arch builds

--- ci-get-unifi-version.sh
+++ ci-get-unifi-version.sh
@@ -4,5 +4,5 @@ set -eu
 
 UNIFI_CHANNEL="$1"
 
-curl -sSL "https://www.ubnt.com/downloads/unifi/debian/dists/${UNIFI_CHANNEL}/ubiquiti/binary-amd64/Packages.gz" \
+curl -sSL "https://www.ubnt.com/downloads/unifi/debian/dists/${UNIFI_CHANNEL}/ubiquiti/binary-${ARG_ARCH:-amd64}/Packages.gz" \
   | zgrep Version | sed -rn 's/Version: ([[:digit:]].[[:digit:]].[[:digit:]]+)-.*/\1/p'
