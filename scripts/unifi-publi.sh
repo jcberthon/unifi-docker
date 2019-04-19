aa3861eb7534cac3e1a2925e5e12a5473f6042e0
Jean-Christophe Berthon <huygens@berthon.eu>
2019-04-20 00:26:51 +0200

    script: Correct bug with UNIFI_VERSION decl

--- scripts/unifi-publi.sh
+++ scripts/unifi-publi.sh
@@ -14,7 +14,7 @@ function bye {
 }
 
 
-opt_channel=0; channel=stable
+channel=stable
 opt_extratag=0
 opt_tag=0
 opt_commit=0
@@ -22,14 +22,14 @@ opt_commit=0
 while getopts c:e:g:t: opt
 do
 	case $opt in
-	c) opt_channel=1; channel=$OPTARG ;;
+	c) channel=$OPTARG ;;
   e) opt_extratag=1; extratag=$OPTARG ;;
   g) opt_commit=1; commit=$OPTARG ;;
 	t) opt_tag=1; tag=$OPTARG ;;
 	?) bye "Option not understood" ;;
 	esac
 done
-shift $(( $OPTIND - 1 ))
+shift $(( OPTIND - 1 ))
 if (( $# )); then
 	repo_name=$1
 	shift
@@ -44,17 +44,17 @@ fi
 (( ! opt_tag )) && tag=${channel}
 
 # Push full version + short commit ID (e.g. 5.10.21-s12345678) if possible
+VERSION_UNIFI="$(./scripts/ci-get-unifi-version.sh "${channel}")"
 if (( opt_commit )); then
-  short_commit="$(echo ${commit} | cut -b-8)"
+  short_commit="$(echo "${commit}" | cut -b-8)"
   docker push "${repo_name}:${VERSION_UNIFI}-s${short_commit}"
 fi
 
 # Push full version (e.g. 5.10.21)
-VERSION_UNIFI="$(./scripts/ci-get-unifi-version.sh ${channel})"
 docker push "${repo_name}:${VERSION_UNIFI}"
 
 # Push major.minor version (e.g. 5.10)
-VERSION_UNIFI_BRANCH="$(echo ${VERSION_UNIFI} | cut -d. -f-2)"
+VERSION_UNIFI_BRANCH="$(echo "${VERSION_UNIFI}" | cut -d. -f-2)"
 docker push "${repo_name}:${VERSION_UNIFI_BRANCH}"
 
 # Push default tag
